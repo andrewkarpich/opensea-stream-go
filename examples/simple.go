345882cb693b1f997262f6df2d19e736ba6e1e9c
Andrew Karpich <andrewkarpich@gmail.com>
2023-10-18 19:38:06 +0200

    Open On method, add phx logger

--- examples/simple.go
+++ examples/simple.go
@@ -10,75 +10,99 @@ import (
 )
 
 func main() {
-	client := opensea.NewStreamClient(types.MAINNET, "apikey", phx.LogInfo, func(err error) {
-		fmt.Println("opensea.NewStreamClient err:", err)
-	})
+
+	client := opensea.NewStreamClient(
+		types.MAINNET, "apikey", phx.NewSimpleLogger(phx.LogInfo), func(err error) {
+			fmt.Println("opensea.NewStreamClient err:", err)
+		},
+	)
 	if err := client.Connect(); err == nil {
 		fmt.Println("client.Connect err:", err)
 		return
 	}
 
-	client.OnItemListed("collection-slug", func(response any) {
-		var itemListedEvent entity.ItemListedEvent
-		err := mapstructure.Decode(response, &itemListedEvent)
-		if err != nil {
-			fmt.Println("mapstructure.Decode err:", err)
-		}
-		fmt.Printf("%+v\n", itemListedEvent)
-	})
+	client.OnItemListed(
+		"collection-slug", func(response any) {
+			var itemListedEvent entity.ItemListedEvent
+			err := mapstructure.Decode(response, &itemListedEvent)
+			if err != nil {
+				fmt.Println("mapstructure.Decode err:", err)
+			}
+			fmt.Printf("%+v\n", itemListedEvent)
+		},
+	)
 
-	client.OnItemSold("collection-slug", func(response any) {
-		var itemSoldEvent entity.ItemSoldEvent
-		err := mapstructure.Decode(response, &itemSoldEvent)
-		if err != nil {
-			fmt.Println("mapstructure.Decode err:", err)
-		}
-		fmt.Printf("%+v\n", itemSoldEvent)
-	})
+	client.OnItemSold(
+		"collection-slug", func(response any) {
+			var itemSoldEvent entity.ItemSoldEvent
+			err := mapstructure.Decode(response, &itemSoldEvent)
+			if err != nil {
+				fmt.Println("mapstructure.Decode err:", err)
+			}
+			fmt.Printf("%+v\n", itemSoldEvent)
+		},
+	)
 
-	client.OnItemTransferred("collection-slug", func(response any) {
-		var itemTransferredEvent entity.ItemTransferredEvent
-		err := mapstructure.Decode(response, &itemTransferredEvent)
-		if err != nil {
-			fmt.Println("mapstructure.Decode err:", err)
-		}
-		fmt.Printf("%+v\n", itemTransferredEvent)
-	})
+	client.OnItemTransferred(
+		"collection-slug", func(response any) {
+			var itemTransferredEvent entity.ItemTransferredEvent
+			err := mapstructure.Decode(response, &itemTransferredEvent)
+			if err != nil {
+				fmt.Println("mapstructure.Decode err:", err)
+			}
+			fmt.Printf("%+v\n", itemTransferredEvent)
+		},
+	)
 
-	client.OnItemCancelled("collection-slug", func(response any) {
-		var itemCancelledEvent entity.ItemCancelledEvent
-		err := mapstructure.Decode(response, &itemCancelledEvent)
-		if err != nil {
-			fmt.Println("mapstructure.Decode err:", err)
-		}
-		fmt.Printf("%+v\n", itemCancelledEvent)
-	})
+	client.OnItemCancelled(
+		"collection-slug", func(response any) {
+			var itemCancelledEvent entity.ItemCancelledEvent
+			err := mapstructure.Decode(response, &itemCancelledEvent)
+			if err != nil {
+				fmt.Println("mapstructure.Decode err:", err)
+			}
+			fmt.Printf("%+v\n", itemCancelledEvent)
+		},
+	)
 
-	client.OnItemReceivedBid("collection-slug", func(response any) {
-		var itemReceivedBidEvent entity.ItemReceivedBidEvent
-		err := mapstructure.Decode(response, &itemReceivedBidEvent)
-		if err != nil {
-			fmt.Println("mapstructure.Decode err:", err)
-		}
-		fmt.Printf("%+v\n", itemReceivedBidEvent)
-	})
-	client.OnItemReceivedOffer("collection-slug", func(response any) {
-		var itemReceivedOfferEvent entity.ItemReceivedOfferEvent
-		err := mapstructure.Decode(response, &itemReceivedOfferEvent)
-		if err != nil {
-			fmt.Println("mapstructure.Decode err:", err)
-		}
-		fmt.Printf("%+v\n", itemReceivedOfferEvent)
-	})
+	client.OnItemReceivedBid(
+		"collection-slug", func(response any) {
+			var itemReceivedBidEvent entity.ItemReceivedBidEvent
+			err := mapstructure.Decode(response, &itemReceivedBidEvent)
+			if err != nil {
+				fmt.Println("mapstructure.Decode err:", err)
+			}
+			fmt.Printf("%+v\n", itemReceivedBidEvent)
+		},
+	)
+	client.OnItemReceivedOffer(
+		"collection-slug", func(response any) {
+			var itemReceivedOfferEvent entity.ItemReceivedOfferEvent
+			err := mapstructure.Decode(response, &itemReceivedOfferEvent)
+			if err != nil {
+				fmt.Println("mapstructure.Decode err:", err)
+			}
+			fmt.Printf("%+v\n", itemReceivedOfferEvent)
+		},
+	)
 
-	client.OnItemMetadataUpdated("collection-slug", func(response any) {
-		var itemMetadataUpdateEvent entity.ItemMetadataUpdateEvent
-		err := mapstructure.Decode(response, &itemMetadataUpdateEvent)
-		if err != nil {
-			fmt.Println("mapstructure.Decode err:", err)
-		}
-		fmt.Printf("%+v\n", itemMetadataUpdateEvent)
-	})
+	client.OnItemMetadataUpdated(
+		"collection-slug", func(response any) {
+			var itemMetadataUpdateEvent entity.ItemMetadataUpdateEvent
+			err := mapstructure.Decode(response, &itemMetadataUpdateEvent)
+			if err != nil {
+				fmt.Println("mapstructure.Decode err:", err)
+			}
+			fmt.Printf("%+v\n", itemMetadataUpdateEvent)
+		},
+	)
+
+	client.On(
+		[]types.EventType{types.ItemListed, types.ItemTransferred}, "collection-slug", func(response any) {
+			// ...
+			fmt.Printf("%+v\n", response)
+		},
+	)
 
 	select {}
 }
--- opensea/stream.go
+++ opensea/stream.go
@@ -4,17 +4,16 @@ import (
 	"fmt"
 	"github.com/foundVanting/opensea-stream-go/types"
 	"github.com/xiaowang7777/phx"
-	"log"
 	"net/url"
 	"sync"
 )
 
 type StreamClient struct {
 	socket   *phx.Socket
-	channels *sync.Map //map[string]*phx.Channel
+	channels *sync.Map
 }
 
-func NewStreamClient(network types.Network, token string, logLevel phx.LoggerLevel, onError func(error)) *StreamClient {
+func NewStreamClient(network types.Network, token string, logger phx.Logger, onError func(error)) *StreamClient {
 	m := map[types.Network]string{
 		types.MAINNET: "wss://stream.openseabeta.com/socket",
 		types.TESTNET: "wss://testnets-stream.openseabeta.com/socket",
@@ -25,100 +24,107 @@ func NewStreamClient(network types.Network, token string, logLevel phx.LoggerLev
 	socket := phx.NewSocket(endPoint)
 
 	socket.OnError(onError)
-	socket.OnClose(func() {
-		err := socket.Reconnect()
-		if err != nil {
-			onError(err)
-		}
-	})
-	socket.Logger = phx.NewSimpleLogger(logLevel)
+	socket.OnClose(
+		func() {
+			err := socket.Reconnect()
+			if err != nil {
+				onError(err)
+			}
+		},
+	)
+	socket.Logger = logger
 	return &StreamClient{
 		socket:   socket,
 		channels: &sync.Map{},
 	}
 }
 
-func (s StreamClient) Connect() error {
-	fmt.Println("Connecting to socket")
+func (s *StreamClient) Connect() error {
 	return s.socket.Connect()
 }
+
 func (s *StreamClient) Disconnect() error {
-	//s.socket.OnError()
-	fmt.Println("Succesfully disconnected from socket")
+	s.socket.Logger.Println(phx.LogInfo, "socket", "succesfully disconnected from socket")
 	s.channels = &sync.Map{}
 	return s.socket.Disconnect()
 }
-func (s *StreamClient) createChannel(topic string) (channel *phx.Channel) {
-	channel = s.socket.Channel(topic, nil)
-	join, err := channel.Join()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	join.Receive("ok", func(response any) {
-		log.Println("Joined channel:", channel.Topic(), response)
-	})
-	join.Receive("error", func(response any) {
-		log.Println("failed 2 joined channel:", channel.Topic(), response)
-	})
-	s.channels.Store(topic, channel)
-	return
-}
-func (s StreamClient) getChannel(topic string) (channel *phx.Channel) {
 
+func (s *StreamClient) createChannel(topic string) (*phx.Channel, error) {
+	channel := s.socket.Channel(topic, nil)
+	_, err := channel.Join()
+	if err != nil {
+		return nil, err
+	}
+	s.channels.Store(topic, channel)
+	return channel, nil
+}
+
+func (s *StreamClient) getChannel(topic string) (*phx.Channel, error) {
 	value, ok := s.channels.Load(topic)
 	if ok {
-		channel = value.(*phx.Channel)
-		return
+		return value.(*phx.Channel), nil
 	}
 
-	channel = s.createChannel(topic)
-
-	return channel
+	return s.createChannel(topic)
 }
 
-func (s StreamClient) on(eventType types.EventType, collectionSlug string, callback func(payload any)) func() {
+func (s *StreamClient) On(eventsType []types.EventType, collectionSlug string, callback func(payload any)) (func() error, error) {
 	topic := collectionTopic(collectionSlug)
-	fmt.Printf("Fetching channel %s\n", topic)
-	channel := s.getChannel(topic)
-	fmt.Printf("Subscribing to %s events on %s\n", eventType, topic)
-	channel.On(string(eventType), callback)
-	return func() {
-		fmt.Printf("Unsubscribing from %s events on %s\n", eventType, topic)
-		leave, err := channel.Leave()
-		if err != nil {
-			fmt.Println("channel.Leave err:", err)
-		}
-		leave.Receive("ok", func(response any) {
-			s.channels.Delete(topic)
-			fmt.Printf("Succesfully left channel %s listening for %s\n", topic, eventType)
-		})
+	channel, err := s.getChannel(topic)
+	if err != nil {
+		return nil, err
 	}
 
+	for _, eventType := range eventsType {
+		s.socket.Logger.Printf(phx.LogInfo, "channel", "subscribing to %s events on %s\n", eventType, topic)
+		channel.On(string(eventType), callback)
+	}
+
+	return func() error {
+		s.socket.Logger.Println(phx.LogInfo, "channel", "unsubscribing from all events on %s", topic)
+		leave, err := channel.Leave()
+		if err != nil {
+			return err
+		}
+
+		leave.Receive(
+			"ok",
+			func(response any) {
+				s.channels.Delete(topic)
+			},
+		)
+		return nil
+	}, nil
+}
+
+func (s *StreamClient) OnItemListed(collectionSlug string, Callback func(itemListedEvent any)) (func() error, error) {
+	return s.On([]types.EventType{types.ItemListed}, collectionSlug, Callback)
+}
+
+func (s *StreamClient) OnItemSold(collectionSlug string, Callback func(itemSoldEvent any)) (func() error, error) {
+	return s.On([]types.EventType{types.ItemSold}, collectionSlug, Callback)
+}
+
+func (s *StreamClient) OnItemTransferred(collectionSlug string, Callback func(itemTransferredEvent any)) (func() error, error) {
+	return s.On([]types.EventType{types.ItemTransferred}, collectionSlug, Callback)
+}
+
+func (s *StreamClient) OnItemCancelled(collectionSlug string, Callback func(itemCancelledEvent any)) (func() error, error) {
+	return s.On([]types.EventType{types.ItemCancelled}, collectionSlug, Callback)
+}
+
+func (s *StreamClient) OnItemReceivedBid(collectionSlug string, Callback func(itemReceivedBidEvent any)) (func() error, error) {
+	return s.On([]types.EventType{types.ItemReceivedBid}, collectionSlug, Callback)
+}
+
+func (s *StreamClient) OnItemReceivedOffer(collectionSlug string, Callback func(itemReceivedOfferEvent any)) (func() error, error) {
+	return s.On([]types.EventType{types.ItemReceivedOffer}, collectionSlug, Callback)
+}
+
+func (s *StreamClient) OnItemMetadataUpdated(collectionSlug string, Callback func(itemMetadataUpdatedEvent any)) (func() error, error) {
+	return s.On([]types.EventType{types.ItemMetadataUpdated}, collectionSlug, Callback)
 }
 
 func collectionTopic(slug string) string {
 	return fmt.Sprintf("collection:%s", slug)
-}
-func (s StreamClient) OnItemListed(collectionSlug string, Callback func(itemListedEvent any)) func() {
-	return s.on(types.ItemListed, collectionSlug, Callback)
-}
-
-func (s StreamClient) OnItemSold(collectionSlug string, Callback func(itemSoldEvent any)) func() {
-	return s.on(types.ItemSold, collectionSlug, Callback)
-}
-func (s StreamClient) OnItemTransferred(collectionSlug string, Callback func(itemTransferredEvent any)) func() {
-	return s.on(types.ItemTransferred, collectionSlug, Callback)
-}
-func (s StreamClient) OnItemCancelled(collectionSlug string, Callback func(itemCancelledEvent any)) func() {
-	return s.on(types.ItemCancelled, collectionSlug, Callback)
-}
-func (s StreamClient) OnItemReceivedBid(collectionSlug string, Callback func(itemReceivedBidEvent any)) func() {
-	return s.on(types.ItemReceivedBid, collectionSlug, Callback)
-}
-func (s StreamClient) OnItemReceivedOffer(collectionSlug string, Callback func(itemReceivedOfferEvent any)) func() {
-	return s.on(types.ItemReceivedOffer, collectionSlug, Callback)
-}
-func (s StreamClient) OnItemMetadataUpdated(collectionSlug string, Callback func(itemMetadataUpdatedEvent any)) func() {
-	return s.on(types.ItemMetadataUpdated, collectionSlug, Callback)
 }
